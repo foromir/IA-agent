bc8ff682546e56e1059573bb63f0f58d5583149c
Mikhail Kalugin <mikhailkalugin@Mikhails-MacBook-Pro-2.local>
2025-03-30 18:26:06 +0200

    add admin

---
+++ src/app/auth/dashboard/layout.tsx
@@ -0,0 +1,20 @@
+"use client"
+import Navbar from "@/app/components/Navbar";
+
+interface DashboardLayoutProps {
+    children: React.ReactNode;
+}
+
+const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
+    return (
+        <div className="min-h-screen bg-gradient-to-br from-purple-50 to-indigo-50 dark:from-gray-900 dark:to-gray-800">
+            <Navbar />
+            <div className="container mx-auto p-6 space-y-8">    
+                {/* Main Content */}
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default DashboardLayout; 
